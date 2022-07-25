e145f2084701d5109bfd371f3a250673dcf2d1ee
Mrcuong1996 <vanlytuongtuluv@gmail.com>
2022-07-25 10:27:12 +0700

    cuong25/07-1

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const { startup } = require('./startup');
+const path = require("path")
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use("/publics", express.static(path.join(__dirname, 'publics')));
+app.use("/views", express.static(path.join(__dirname, 'views')));
 
 startup(app);
 
