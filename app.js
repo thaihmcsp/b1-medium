25dd3cefb47d9e59e47c564080543a52357e41f3
Mrcuong1996 <vanlytuongtuluv@gmail.com>
2022-07-25 10:39:46 +0700

    cuong25/07-2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const { startup } = require('./startup');
 const path = require("path")
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const path = require('path');
 
 const app = express();
 
