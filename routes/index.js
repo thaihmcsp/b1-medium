9a41da4a2beed0d37fe8701fc575cdbaf7372f45
thaidd7b <thaidd7b@gmail.com>
2022-07-25 09:16:32 +0700

    pull admin profile

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('123456')
+    res.render('pages/admin/profileAdmin/profileAdmin');
 })
 
 module.exports = router;
