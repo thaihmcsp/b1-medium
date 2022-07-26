2f56c52f7b9b97cd171ec34f14ec686200e4113a
tranhuuphuocks2018 <tranhuuphuocks2018@gmail.com>
2022-07-26 16:39:11 +0700

    home-ui and some adjustment

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/publics',(express.static(path.join(__dirname,'./publics'))))
+app.use('/views',(express.static(path.join(__dirname,'./views'))))
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('123456')
+    res.render('./pages/user/home/Home')
+    //res.send('123456')
 })
 
 module.exports = router;
