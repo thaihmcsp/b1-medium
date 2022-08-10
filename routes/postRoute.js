eb2987a64042cbaaa89fd7e583b2cbe291497ceb
Đề Toàn Lê Trọng <letrongdetoan@gmail.com>
2022-08-10 09:43:39 +0700

    xong create post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,3 +1,23 @@
+const { Post } = require('../models/Post');
 const router = require('express').Router();
+
+
+router.get('/createPost', (req, res) => {
+    res.render('pages/user/createPost/createPost')
+})
+router.post('/createPost', async (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    let authorId = req.body.authorId;
+    try {
+        let data = await Post.create({
+            title, content, authorId
+        })
+        console.log(16, data)
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
 module.exports = router;
